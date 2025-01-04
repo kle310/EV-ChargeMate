444c53d228a90196dabab2c0d6ceb9c07ae94c48
kle310 <khacle@icloud.com>
2025-01-04 12:58:28 -0800

    make test pass

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from "@playwright/test";
 
 test("homepage has title", async ({ page }) => {
   await page.goto("/");
-  await expect(page).toHaveTitle("Free SoCal Fast Chargerss");
+  await expect(page).toHaveTitle("Free SoCal Fast Chargers");
 });
